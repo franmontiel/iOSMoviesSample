6e47954d0e40db43a0a329b02f5e6d60baa5ebe1
Fran Montiel <me@franmontiel.com>
2017-06-19 18:55:26 +0200

    Create simple table view

---
+++ iOSMoviesSample/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  iOSMoviesSample
+//
+//  Created by fj on 17/06/2017.
+//  Copyright © 2017 Spiral Apps. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+
+    var id: Int
+    var title: String
+    var overview: String
+    var poster: URL
+    
+    
+}
---
+++ iOSMoviesSample/UpcomingMoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UpcomingMoviesViewCell.swift
+//  iOSMoviesSample
+//
+//  Created by fj on 19/06/2017.
+//  Copyright © 2017 Spiral Apps. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingMoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
